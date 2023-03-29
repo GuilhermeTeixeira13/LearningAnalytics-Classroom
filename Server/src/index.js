895003613697eeb554e1a8660b71b2413f0fc95a
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2023-03-29 15:52:49 +0100

    Everything working! Requests are now possible.

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,7 @@
-
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
 const app = express();
 app.use(express.static('static'));
 app.use(express.json());
@@ -9,25 +11,31 @@ let registos = [];
 // APLICAR MIDDLEWARES
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/table/:tablenumber/:studentnumber/:machineId', (req, res) => {
-	console.log("recebi um request");
-	let studentnumber =req.params.studentnumber;
-	let tablenumber = req.params.tablenumber;
-	let machineId = req.params.machineId;
-	
-	const registo = {"data":"27-03-2023", "hora":"11:14", "aula":"Redes de Computadores", "num":studentnumber, "table":tablenumber, "idMaquina":machineId};
-	registos.push(registo);
-	
-	//DATA SER ENVIADA PARA UMA BD LOCAL OU CLOUD
-	return res.status(200).json(registo);
+  let studentnumber =req.params.studentnumber;
+  let tablenumber = req.params.tablenumber;
+  let machineId = req.params.machineId;
+  
+  const registo = {"data":"27-03-2023", "hora":"11:14", "aula":"Redes de Computadores", "num":studentnumber, "table":tablenumber, "idMaquina":machineId};
+  registos.push(registo);
+  
+  //DATA SER ENVIADA PARA UMA BD LOCAL OU CLOUD
+  return res.status(200).json(registo);
 });
 
 app.get('/table' , (req, res) => {
-	const allRegistos = registos;
-	return res.status(200).json(registos);	
+  const allRegistos = registos;
+  return res.status(200).json(registos); 
 });
 
-app.listen(3333, () => console.log('Server is running'));
+const options = {
+  key: fs.readFileSync('/home/guilherme/Desktop/IoT_Attendance_Project/Server/src/server.key'),
+  cert: fs.readFileSync('/home/guilherme/Desktop/IoT_Attendance_Project/Server/src/server.cert')
+};
+
+https.createServer(options, app).listen(3333, () => {
+  console.log('Server is running on port 3333');
+});
