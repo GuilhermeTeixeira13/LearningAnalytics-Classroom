46f60e6fbae5b8cf9a0f2a049f8f944713106713
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2023-03-29 14:31:23 +0100

    Students app POST test

--- Server/src/index.js
+++ Server/src/index.js
@@ -4,26 +4,30 @@ const app = express();
 app.use(express.static('static'));
 app.use(express.json());
 
+let registos = [];
+
 // APLICAR MIDDLEWARES
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/table/:studentnumber/:tablenumber', (req, res) => {
-	let studentnumber=req.params.studentnumber;
-	let tablenumber= req.params.tablenumber;
+app.post('/table/:tablenumber/:studentnumber/:machineId', (req, res) => {
+	console.log("recebi um request");
+	let studentnumber =req.params.studentnumber;
+	let tablenumber = req.params.tablenumber;
+	let machineId = req.params.machineId;
 	
-	let data= {
-		"data":"27-03-2023",
-		"hora": "11:14",
-		"class":"Redes de Computadores",
-		"stdNumb":studentnumber,
-		"tableN":tablenumber
-	}
+	const registo = {"data":"27-03-2023", "hora":"11:14", "aula":"Redes de Computadores", "num":studentnumber, "table":tablenumber, "idMaquina":machineId};
+	registos.push(registo);
 	
 	//DATA SER ENVIADA PARA UMA BD LOCAL OU CLOUD
-	res.send(data)
+	return res.status(200).json(registo);
+});
+
+app.get('/table' , (req, res) => {
+	const allRegistos = registos;
+	return res.status(200).json(registos);	
 });
 
 app.listen(3333, () => console.log('Server is running'));
