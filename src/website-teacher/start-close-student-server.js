ae5d19f9a75bd5e857d516ae4fad54941263357d
Guilherme Teixeira <guilherme.teixeira@ubi.pt>
2023-04-12 17:32:47 +0100

    Testing cookies to synchronize the button state between multiple devices

--- src/website-teacher/start-close-student-server.js
+++ src/website-teacher/start-close-student-server.js
@@ -1,26 +1,53 @@
-function startStudentServer(){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', '/start-new-server');
-    xhr.onload = function() {
-	console.log(xhr.responseText);
-    };
-    xhr.send();	
-    
-    const startBtn = document.getElementById("start-server");
-    startBtn.style.display = "none";
-    
-    const closeBtn = document.getElementById("close-server");
-    closeBtn.style.display = "flex";
+function startServer() {
+    // Make an AJAX request to start the server
+    fetch('/start-new-server')
+        .then(response => {
+            // If the request is successful, set a cookie with a value of "running"
+            document.cookie = "serverState=running";
+            // Update the button styles
+            const startBtn = document.getElementById("start-server");
+            startBtn.style.display = "none";
+            const closeBtn = document.getElementById("close-server");
+            closeBtn.style.display = "flex";
+        })
+        .catch(error => {
+            console.error('Error starting server:', error);
+        });
 }
 
-function closeStudentServer(){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', '/stop-server');
-    xhr.onload = function() {
-	console.log(xhr.responseText);
-    };
-    xhr.send();	
-    
-    const closeBtn = document.getElementById("close-server");
-    closeBtn.style.display = "none";
+function closeServer() {
+    // Make an AJAX request to stop the server
+    fetch('/stop-server')
+        .then(response => {
+            // If the request is successful, set a cookie with a value of "stopped"
+            document.cookie = "serverState=stopped";
+            // Update the button styles
+            const startBtn = document.getElementById("start-server");
+            startBtn.style.display = "flex";
+            const closeBtn = document.getElementById("close-server");
+            closeBtn.style.display = "none";
+        })
+        .catch(error => {
+            console.error('Error stopping server:', error);
+        });
+}
+
+// Check the value of the "serverState" cookie when the page loads
+window.addEventListener('load', () => {
+    const serverState = getCookie('serverState');
+    if (serverState === 'running') {
+        const startBtn = document.getElementById("start-server");
+        startBtn.style.display = "none";
+        const closeBtn = document.getElementById("close-server");
+        closeBtn.style.display = "flex";
+    }
+});
+
+// Helper function to get the value of a cookie by name
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift();
+    }
 }
