5633bd47b5675d428a351cc0aab208b7a670d0cb
GuilhermeTeixeira13 <guilherme.teixeira@ubi.pt>
2023-04-16 00:18:33 +0100

    Added TextField so the teacher can enter the class name.

--- src/website-teacher/start-close-student-server.js
+++ src/website-teacher/start-close-student-server.js
@@ -1,53 +1,49 @@
 // Check the server state when the page loads
 window.addEventListener('load', checkServerState);
 
-function startStudentServer() {
-    // Make an AJAX request to start the server
-    fetch('/start-new-server')
-        .then(response => {
-            // Update the server state
-            updateServerState('running');
+function toggleServer(state) {
+    const errorDiv = document.querySelector('#error-msg');
+    const startBtn = document.getElementById('start-server');
+    const closeBtn = document.getElementById('close-server');
+    const classLabel= document.getElementById('class-label');
+    const className = document.getElementById('class-name');
+    
+    // Update the server state
+    updateServerState(state);
 
-            // Update the button styles
-            const startBtn = document.getElementById('start-server');
-            startBtn.style.display = 'none';
-            const closeBtn = document.getElementById('close-server');
-            closeBtn.style.display = 'block';
-        })
-        .catch(error => {
-            console.error('Error starting server:', error);
-        });
+    // Update the button styles
+    startBtn.style.display = (state === 'running') ? 'none' : 'block';
+    closeBtn.style.display = (state === 'running') ? 'block' : 'none';
+    errorDiv.style.display = (state === 'running') ? 'none' : 'block';
+    className.readOnly = (state === 'running') ? true : false;
+    classLabel.innerHTML = (state === 'running') ? 'Class currently running:' : 'Enter class name:';
+
+}
+
+function startStudentServer() {
+    const errorDiv = document.querySelector('#error-msg');
+    if (emptyClassName()) {
+        errorDiv.innerHTML = "The class name can't be empty!";
+    } else {
+        errorDiv.innerHTML = "";
+        // Make an AJAX request to start the server
+        fetch('/start-new-server')
+            .then(response => toggleServer('running'))
+            .catch(error => console.error('Error starting server:', error));
+        }
 }
 
 function stopStudentServer() {
     // Make an AJAX request to stop the server
     fetch('/stop-server')
-        .then(response => {
-            // Update the server state
-            updateServerState('stopped');
-
-            // Update the button styles
-            const startBtn = document.getElementById('start-server');
-            startBtn.style.display = 'block';
-            const closeBtn = document.getElementById('close-server');
-            closeBtn.style.display = 'none';
-        })
-        .catch(error => {
-            console.error('Error stopping server:', error);
-        });
+    .then(response => toggleServer('stopped'))
+    .catch(error => console.error('Error stopping server:', error));
 }
 
 function checkServerState() {
     fetch('/server-state')
-        .then(response => response.text())
-        .then(state => {
-            if (state === 'running') {
-                const startBtn = document.getElementById('start-server');
-                startBtn.style.display = 'none';
-                const closeBtn = document.getElementById('close-server');
-                closeBtn.style.display = 'block';
-            }
-        });
+    .then(response => response.text())
+    .then(state => toggleServer(state));
 }
 
 function updateServerState(state) {
@@ -60,3 +56,6 @@ function updateServerState(state) {
     });
 }
 
+function emptyClassName() {
+    return document.getElementById("class-name").value.trim() === "";
+}
